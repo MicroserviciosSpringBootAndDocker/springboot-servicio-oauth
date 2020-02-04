7bc33cee4dc4f4301b6a360ceac7cec824c35e45
Jose <rodriguez26@gmail.com>
2020-02-04 13:37:22 -0500

    último cambio

--- src/main/java/com/rodriguez/springboot/app/oauth/services/IUsuarioService.java
+++ src/main/java/com/rodriguez/springboot/app/oauth/services/IUsuarioService.java
@@ -1,7 +1,5 @@
 package com.rodriguez.springboot.app.oauth.services;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.rodriguez.springboot.app.commons.models.entity.Usuario;
 
